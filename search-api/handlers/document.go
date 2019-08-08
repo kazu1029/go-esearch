d2a8205be4c1b3ebaba91341f969f61fb2290252
kazu1029 <imgnt10nxqu05@gmail.com>
2019-08-08 15:16:30 +0900

    move BulkInsert to pkg/esearch

--- search-api/handlers/document.go
+++ search-api/handlers/document.go
@@ -12,26 +12,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/kazu1029/gin-elastic/search-api/pkg/esearch"
 	"github.com/olivere/elastic"
-	"github.com/teris-io/shortid"
 )
 
 const (
 	elasticIndexName = "documents"
 	elasticTypeName  = "document"
 )
-
-type Document struct {
-	ID        string    `json:"id"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
-
-type DocumentRequest struct {
-	Title   string `json:"title"`
-	Content string `json:"content"`
-}
 
 type DocumentResponse struct {
 	Title     string    `json:"title"`
@@ -46,64 +32,6 @@ type SearchResponse struct {
 	Documents []DocumentResponse `json:"documents"`
 }
 
-// index mapping sample
-// const mappingSample = `
-// {
-// 	"settings": {
-// 	  "analysis": {
-// 			"analyzer": {
-// 			  "ja_kuromoji_analyzer": {
-// 					"type": "custom",
-// 					"tokenizer": "kuromoji_tokenizer",
-// 					"filter": [
-// 					  "kuromoji_baseform",
-// 						"kuromoji_part_of_speech",
-// 						"kuromoji_readingform"
-// 					]
-// 				}
-// 			}
-// 		}
-// 	},
-// 	"mappings": {
-// 		"document":{
-// 			"properties":{
-// 				"content": {
-// 					"type": "text",
-// 					"analyzer": "ja_kuromoji_analyzer"
-// 				},
-// 				"title": {
-// 					"type": "text",
-// 					"analyzer": "ja_kuromoji_analyzer"
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-// `
-
-// index template sample is following json
-// const indexTemplate = `
-// {
-//   "index_patterns": ["accesslog-*"],
-// 	"settings": {
-// 	  "number_of_shards": 1
-// 	},
-// 	"mappings": {
-// 		"accesslog_type": {
-// 			"_source": {
-// 				"enabled": true
-// 			},
-// 			"properties": {
-// 				"host": { "type": "keyword" },
-// 				"uri": { "type": "keyword" },
-// 				"method": { "type": "keyword" },
-// 				"accesstime": { "type": "date" }
-// 			}
-//     }
-// 	}
-// }
-// `
-
 func ElasticIndex(client *elastic.Client) *esearch.IndexService {
 	return esearch.NewIndexService(client)
 }
@@ -112,37 +40,26 @@ func CreateDocumentsEndpoint(c *gin.Context) {
 	elasticClient, err := InitElastic()
 	if err != nil {
 		log.Println(err)
+		errorResponse(c, http.StatusBadRequest, err.Error())
 	}
-	var docs []DocumentRequest
+	ctx := context.Background()
+	// TODO: need to dynamic variables
+	var docs []esearch.DocumentRequest
 	if err := c.BindJSON(&docs); err != nil {
 		log.Printf("err is %+v\n", err)
 		errorResponse(c, http.StatusBadRequest, "Malformed request body")
 		return
 	}
 
-	bulk := elasticClient.
-		Bulk().
-		Index(elasticIndexName).
-		Type(elasticTypeName)
-
-	for _, d := range docs {
-		doc := Document{
-			ID:        shortid.MustGenerate(),
-			Title:     d.Title,
-			CreatedAt: time.Now().UTC(),
-			UpdatedAt: time.Now().UTC(),
-			Content:   d.Content,
-		}
-		bulk.Add(elastic.NewBulkIndexRequest().Id(doc.ID).Doc(doc))
-	}
-	if _, err := bulk.Do(c.Request.Context()); err != nil {
-		log.Printf("err is %+v\n", err)
-		errorResponse(c, http.StatusInternalServerError, "Failed to create documents")
+	index := ElasticIndex(elasticClient)
+	res, err := index.BulkInsert(ctx, docs, elasticIndexName, elasticTypeName)
+	if err != nil {
+		errorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Document Created",
+		"message": res,
 	})
 }
 
--- search-api/pkg/esearch/index.go
+++ search-api/pkg/esearch/index.go
@@ -3,12 +3,29 @@ package esearch
 import (
 	"context"
 	"errors"
+	"time"
 
 	"github.com/olivere/elastic"
+	"github.com/teris-io/shortid"
 )
 
 type IndexService struct {
-	Client *elastic.Client
+	Client    *elastic.Client
+	IndexName string
+	TypeName  string
+}
+
+type Document struct {
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type DocumentRequest struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
 func NewIndexService(Client *elastic.Client) *IndexService {
@@ -44,4 +61,26 @@ func (s *IndexService) CreateIndexTemplate(ctx context.Context, templateName str
 		return "", err
 	}
 	return "Template Created", nil
+}
+
+func (s *IndexService) BulkInsert(ctx context.Context, docs []DocumentRequest, indexName string, typeName string) (string, error) {
+	bulk := s.Client.
+		Bulk().
+		Index(indexName).
+		Type(typeName)
+
+	for _, d := range docs {
+		doc := Document{
+			ID:        shortid.MustGenerate(),
+			Title:     d.Title,
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			Content:   d.Content,
+		}
+		bulk.Add(elastic.NewBulkIndexRequest().Id(doc.ID).Doc(doc))
+	}
+	if _, err := bulk.Do(ctx); err != nil {
+		return "", err
+	}
+	return "Document Bulk Created", nil
 }
